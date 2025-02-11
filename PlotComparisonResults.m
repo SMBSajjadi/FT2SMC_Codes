a6a8db0da995322a278e5aa11eb5b13967e4af4a
Mohammad Bagher <138445951+SMBSajjadi@users.noreply.github.com>
2025-02-11 19:22:00 +0330

    Add files via upload

--- PlotComparisonResults.m
+++ PlotComparisonResults.m
@@ -6,7 +6,9 @@ function PlotComparisonResults()
     
     xTSMC = load('xTSMC.mat');
     xSMC = load('xSMC.mat');
-    
+    xDOBBSC = load('xDOBBSC.mat');
+    xDOBBSC = xDOBBSC.x;
+
     xTSMC = xTSMC.X;
     xTSMC(:,end+1) = xTSMC(:,end);
     xSMC = xSMC.xSMC;
@@ -53,11 +55,14 @@ function PlotComparisonResults()
     plot(t,xTSMC(1,:),'b','LineWidth',1.1)
     hold on
     grid on
-    plot(t,xSMC(1,:),'m','LineWidth',1.2)
+    plot(t,xSMC(1,:),'m','LineWidth',1.1)
     hold on
-    plot(t,xd,'r--','LineWidth',1.2)
+    grid on
+    plot(t,xDOBBSC(end-1,:),'g','LineWidth',1.1)
+    hold on
+    plot(t,xd,'r--','LineWidth',1.1)
     xlabel('Time (s)','InterPreter','Latex')
-    legend('F2TSMC','SMC','x_{d}')
+    legend('Proposed','ASMC','DOBBSC','x_{d}')
     ylabel('x(m)','fONTweight','bold')
     xlim([0 70])
     ylim([1 5])
@@ -68,11 +73,13 @@ function PlotComparisonResults()
     plot(t,xTSMC(2,:),'b','LineWidth',1.1)
     hold on
     grid on
-    plot(t,xSMC(2,:),'m','LineWidth',1.2)
+    plot(t,xSMC(2,:),'m','LineWidth',1.1)
     hold on
-    plot(t,yd,'r--','LineWidth',1.2)
+    plot(t,xDOBBSC(end-3,:),'g','LineWidth',1.1)
+    hold on
+    plot(t,yd,'r--','LineWidth',1.1)
     xlabel('Time (s)','InterPreter','Latex')
-    legend('F2TSMC','SMC','y_{d}')
+    legend('Proposed','ASMC','DOBBSC','y_{d}')
     ylabel('y(m)','fONTweight','bold')
     xlim([0 70])
     ylim([1 5])
@@ -83,11 +90,13 @@ function PlotComparisonResults()
     plot(t,xTSMC(3,:),'b','LineWidth',1.1)
     hold on
     grid on
-    plot(t,xSMC(3,:),'m','LineWidth',1.2)
+    plot(t,xSMC(3,:),'m','LineWidth',1.1)
     hold on
-    plot(t,zd,'r--','LineWidth',1.2)
+    plot(t,xDOBBSC(end-5,:),'g','LineWidth',1.1)
+    hold on
+    plot(t,zd,'r--','LineWidth',1.1)
     xlabel('Time (s)','InterPreter','Latex')
-    legend('F2TSMC','SMC','z_{d}')
+    legend('Proposed','ASMC','DOBBSC','z_{d}')
     ylabel('z(m)','fONTweight','bold')
     xlim([0 70])
     ylim([-1 4])
@@ -98,11 +107,13 @@ function PlotComparisonResults()
     plot(t,xTSMC(4,:),'b','LineWidth',1.1)
     hold on
     grid on
-    plot(t,xSMC(4,:),'m','LineWidth',1.2)
+    plot(t,xSMC(4,:),'m','LineWidth',1.1)
     hold on
-    plot(t,psid,'r--','LineWidth',1.2)
+    plot(t,xDOBBSC(5,:),'g','LineWidth',1.1)
+    hold on
+    plot(t,psid,'r--','LineWidth',1.1)
     xlabel('Time (s)','InterPreter','Latex')
-    legend('F2TSMC','SMC','\psi_{d}')
+    legend('Proposed','ASMC','DOBBSC','\psi_{d}')
     ylabel('\psi(rad)','fONTweight','bold')
     xlim([0 70])
     ylim([-1 1])
@@ -111,18 +122,20 @@ function PlotComparisonResults()
 
     f2 = figure(2);
 
-    plot3(xTSMC(1,:),xTSMC(2,:),xTSMC(3,:),'r','LineWidth',1.3)
+    plot3(xTSMC(1,:),xTSMC(2,:),xTSMC(3,:),'b','LineWidth',1.1)
     hold on
     grid on
-    plot3(xSMC(1,:),xSMC(2,:),xSMC(3,:),'b','LineWidth',1.2)
+    plot3(xSMC(1,:),xSMC(2,:),xSMC(3,:),'m','LineWidth',1.1)
     hold on
-    plot3(xd,yd,zd,'k--','LineWidth',1.2)
+    plot3(xDOBBSC(end-1,:),xDOBBSC(end-3,:),xDOBBSC(end-5,:),'g','LineWidth',1.1)
+    hold on
+    plot3(xd,yd,zd,'r--','LineWidth',1.1)
 
     xlabel('x(m)','InterPreter','Latex')
     ylabel('y(m)','InterPreter','LATEX')
     zlabel('z(m)','InterPreter','lATex')
 
-    legend('F2TSMC Trajectory','SMC Trajectory','Target Trajectory')
+    legend('Proposed ','ASMC ','DOBBSC ','Target Trajectory')
 
     movegui(f1,'east')
     movegui(f2,'west')
